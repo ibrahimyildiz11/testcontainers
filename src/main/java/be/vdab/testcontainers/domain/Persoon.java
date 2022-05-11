37944abfb2e6f03c7ffc739c384dbd0f992c312c
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-11 11:41:52 +0000

    testcontainer

--- src/main/java/be/vdab/testcontainers/domain/Persoon.java
+++ src/main/java/be/vdab/testcontainers/domain/Persoon.java
@@ -1,2 +1,19 @@
-package be.vdab.testcontainers.domain;public class Persoon {
+package be.vdab.testcontainers.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "personen")
+public class Persoon {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String voornaam;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
 }
--- src/main/java/be/vdab/testcontainers/repositories/PersoonRepository.java
+++ src/main/java/be/vdab/testcontainers/repositories/PersoonRepository.java
@@ -1,2 +1,7 @@
-package be.vdab.testcontainers.repositories;public interface PersoonRepository {
+package be.vdab.testcontainers.repositories;
+
+import be.vdab.testcontainers.domain.Persoon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersoonRepository extends JpaRepository<Persoon, Long> {
 }
